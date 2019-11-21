a99797325f4e710ccb5de10df084a73fac72f031
hoferj <55890926+Hoferj867@users.noreply.github.com>
2019-11-21 20:44:46 +0300

    Add files via upload

---
+++ Laba_3_1/Laba_3_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Laba_3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите количество элементов матрицы:");
+            int N = int.Parse(Console.ReadLine());
+            int[] nums = new int[N];
+            Random rand = new Random();
+            int lim = 1;
+            for (int i = 0; i < N; i++)
+            {
+                nums[i] = rand.Next(-30, 45);
+                if (lim > 10)
+                {
+                    Console.Write("\n\n");
+                    lim = 1;
+                }
+                lim++;
+                Console.Write("{0,5} ", nums[i]);
+            }
+            Console.Write("\n\n");
+            Array.Reverse(nums);
+                for ( int i = 0; i < N; i++)
+                {
+                    if (nums[i] < 0)
+                    {
+                        continue;
+                    }
+                    if (lim > 10)
+                    {
+                        Console.Write("\n\n");
+                        lim = 1;
+                    }
+                    lim++;
+                    Console.Write("{0,5}",nums[i]);
+                    }
+                }
+        }
+    }
+
+        
+ 
---
+++ Laba_3_3/Laba_3_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Laba_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите кол-во циклических сдвигов: ");
+            int k = Convert.ToInt32(Console.Read());
+            Console.WriteLine();
+            Console.Write("Введите длину массива: ");
+            int m = Convert.ToInt32(Console.Read());
+            int[] nums = new int[m];
+            Console.WriteLine();
+            Random rand = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                nums[i] = rand.Next(100);
+                Console.Write ("{0, 2} ", nums[i]);
+            }
+            Console.Write("\n\n");
+            if (k > nums.Length)
+                k = k % 10;
+            for (int i = k; i < nums.Length; i++)
+            {
+                Console.Write("{0, 2} ", nums[i]);
+            }
+            for (int i = 0; i < k; i++)
+            {
+                Console.Write("{0, 2} ", nums[i]);
+            }
+            Console.ReadKey();
+        }
+
+    }
+
+}

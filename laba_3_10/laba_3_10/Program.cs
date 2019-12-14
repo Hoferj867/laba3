881a188dd3f5146a78dd9f4dad0f2f803e052a0e
hoferj <55890926+Hoferj867@users.noreply.github.com>
2019-12-14 09:50:47 +0300

    Add files via upload

---
+++ laba_3_10/laba_3_10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace laba_3_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число элементов:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] A = new int[n];
+            int max = 0;
+            int i = 0;
+            Random rand = new Random();
+            for ( i = 0; i < A.GetLength(0); i++)
+            {
+                A[i] = rand.Next(-10, 10);
+                Console.Write("{0,2}", A[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine(maxRevers(A, i, max));
+            static int maxRevers(int[] A, int i, int max)
+            {
+                for (i = 1; i < A.Length; i+=2)
+                {
+                        if(max < A[i] && A[i]%2!=0)
+                        {
+                            max = A[i];
+                        }
+  
+
+                }
+                return max;
+            }
+        }            
+    }
+}
